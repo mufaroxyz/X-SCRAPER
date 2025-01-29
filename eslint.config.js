48ac7aca4d70595a83db803d2fd8d51bc9ca8ef7
DzikStar <dawid0mila@gmail.com>
2025-01-29 15:19:50 +0100

    Prettier Adjust to ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,25 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+	{
+		files: ['**/*.{js,mjs,cjs,ts}'],
+		languageOptions: {
+			globals: globals.browser,
+			parser: tsParser,
+			sourceType: 'module',
+		},
+		plugins: {
+			'@typescript-eslint': tsPlugin,
+		},
+		rules: {
+			...tsPlugin.configs.recommended.rules,
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
 ];
