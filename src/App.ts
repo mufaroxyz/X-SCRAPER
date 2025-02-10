1f8d7bb11b88ff464e616ab30706aaa1b6b9af15
DzikStar <dawid0mila@gmail.com>
2025-02-10 22:52:53 +0100

    saveFile and rmdir functions

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-import { clearFolder, saveFile } from './utils/FileManager.js';
+import { clearPath, saveFile } from './utils/FileManager.js';
 
 export default class Xscraper {
 	async run() {}
--- src/utils/FileManager.ts
+++ src/utils/FileManager.ts
@@ -1,6 +1,34 @@
-import * as fs from 'fs';
+import * as fs from 'node:fs';
 import * as path from 'path';
 
-export function clearFolder(target: string): void {}
+export async function clearPath(targetPath: string): Promise<void> {
+    try {
+        const entries = await fs.promises.readdir(targetPath, { withFileTypes: true });
 
-export function saveFile(target: string, name: string): void {}
+        for (const entry of entries) {
+            const fullPath = path.join(targetPath, entry.name);
+
+            if (entry.isDirectory()) {
+              await clearPath(fullPath);
+              await fs.promises.rmdir(fullPath);
+            } else {
+              await fs.promises.unlink(fullPath);
+            }
+          }
+    } catch (error) {
+        console.log(`[ERROR] PATH NOT CLEARED ${error}`)
+    }
+}
+
+export async function saveFile(fileName: string, fileContent: string, filePath: string = './'): Promise<void> {
+	try {
+		await fs.promises.mkdir(filePath, { recursive: true });
+		await fs.writeFile(`${filePath}${fileName}`, fileContent, err => {
+			if (err) {
+				console.error(err);
+			}
+		});
+	} catch (error) {
+		console.log(`[ERROR] FILE NOT SAVED: ${error}`);
+	}
+}
